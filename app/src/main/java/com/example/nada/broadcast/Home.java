57186e8567eb1765f962109e03b969ff31d3fbd0
ShanzayA <shanzayamjad@hotmail.com>
2016-07-06 15:18:07 -0400

    Navigation Bar Preliminary Setup

--- app/src/main/java/com/example/nada/broadcast/Home.java
+++ app/src/main/java/com/example/nada/broadcast/Home.java
@@ -1,16 +1,93 @@
 package com.example.nada.broadcast;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.Menu;
-        import android.view.MenuItem;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class Home extends AppCompatActivity {
+public class Home extends FragmentActivity {
+
+    BrowseFragment browse = new BrowseFragment();
+    ListeningFragment listening = new ListeningFragment();
+    FavouritesFragment favourites = new FavouritesFragment();
+    ProfileFragment profile = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+//        Toolbar titlebar = (Toolbar) findViewById(getResources().getIdentifier("action_bar_title", "id", "android"));
+//        setSupportActionBar(titlebar);
+
+
+        if (findViewById(R.id.fragcontent) != null){
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            browse.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction().add(R.id.fragcontent, browse).commit();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()){
+//            case R.id.profile:
+//                break;
+//        }
+//        return true;
+//    }
+
+    public void toBrowsePage(View view){
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragcontent, browse);
+        transaction.addToBackStack(null); //this might be unnecessarily accumulating info and might slow down app. When does it pop? There is endless backs available
+        transaction.commit();
+    }
+
+    public void toListeningPage(View view){
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragcontent, listening);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void toRecordActivity (View view){
+
+        Intent record = new Intent(Home.this, Record.class);
+        startActivity(record);
+    }
+
+    public void toFavouritesPage(View view){
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragcontent, favourites);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void toProfilePage(View view){
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragcontent, profile);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/nada/broadcast/Record.java
@@ -0,0 +1,133 @@
+package com.example.nada.broadcast;
+
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.media.MediaRecorder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+//import com.roughike.bottombar.BottomBar;
+//import com.roughike.bottombar.OnMenuTabSelectedListener;
+//import android.support.design.widget.CoordinatorLayout;
+
+import java.io.IOException;
+
+public class Record extends AppCompatActivity {
+
+//    private CoordinatorLayout coordinatorLayout;
+
+    private static MediaRecorder recorder;
+    private static MediaPlayer player;
+
+    private static String filename;
+    private static Button stopButton;
+    private static Button playButton;
+    private static ImageButton recordButton;
+
+    private boolean isRecording = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        stopButton = (Button) findViewById(R.id.stopButton);
+        playButton = (Button) findViewById(R.id.playButton);
+        recordButton = (ImageButton) findViewById(R.id.recordButton);
+
+        filename = Environment.getExternalStorageDirectory().getAbsolutePath() + "/myaudio.3gp";
+
+    }
+
+//    private void onRecord(boolean start){
+//        if (start){
+//            startRecording();
+//        }
+//        else{
+//            stopRecording();
+//        }
+//    }
+//
+//    private void onPlay(boolean start){
+//        if (start){
+//            startPlaying();
+//        }
+//        else{
+//            stopPlaying();
+//        }
+//    }
+
+    public void startRecording(View view) throws IOException{
+        isRecording = true;
+        stopButton.setEnabled(true);
+        playButton.setEnabled(false);
+        recordButton.setEnabled(false);
+
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(filename);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try{
+            recorder.prepare();
+        }
+        catch (IOException e){
+            Log.e("RecordTest", "prepare() failed");
+        }
+
+        recorder.start();
+    }
+
+    public void stopRecording(View view){
+
+        stopButton.setEnabled(false);
+        playButton.setEnabled(true);
+
+        if (isRecording){
+            recordButton.setEnabled(false);
+            recorder.stop();
+            recorder.release();
+            recorder = null; //free memory space?
+            isRecording = false;
+        }
+        else {
+            player.release();
+            player = null;
+            recordButton.setEnabled(true);
+        }
+    }
+
+    public void startPlaying(View view) throws IOException{
+        playButton.setEnabled(false);
+        recordButton.setEnabled(false);
+        stopButton.setEnabled(true);
+
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(filename);
+            player.prepare();
+            player.start();
+        }
+        catch (IOException e){
+            Log.e("AudioTest", "prepare() failed");
+        }
+    }
+
+
+
+
+//
+//    public void stopPlaying(View view) {
+//        player.release();
+//        player = null;
+//    }
+
+}
